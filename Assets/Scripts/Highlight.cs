11a3a7fd0e1124b568147e32b598c2490eba7492
TuanTer <tuanterth@gmail.com>
2024-01-03 00:20:05 +0700

    update build

--- Assets/Scripts/Highlight.cs
+++ Assets/Scripts/Highlight.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Highlight : MonoBehaviour
 {
     public static Highlight instance;
-
+    public  Text uname;
+    void Start()
+    {
+        Debug.Log("111>" + PlayerPrefs.GetString("uname"));
+        uname.text = PlayerPrefs.GetString("uname");
+    }
     private int[,] highlight_line = new int[9, 9] { 
         {0, 1, 2,   3, 4, 5,    6, 7, 8 },
         {9,10,11,   12,13,14,   15,16,17},
--- Assets/Scripts/Menu_Buttons.cs
+++ Assets/Scripts/Menu_Buttons.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu_Buttons : MonoBehaviour
 {
-    public GameObject login;
-    public GameObject signIn;
+    public GameObject loginObj;
+    public GameObject signinObj;
+    public GameObject loginBtn;
+    public GameObject signinBtn;
+    public Text warnLogin;
+    public Text warning;
+    public InputField uname;
+    public InputField pass;
+    public InputField rUname;
+    public InputField rPass;
+    public InputField rPassConfirm;
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
@@ -45,6 +55,33 @@ public class Menu_Buttons : MonoBehaviour
     public void Set_Pause_Game(bool paused_game)
     {
         Game_Settings.Instance.Set_Pause(paused_game);
+    }
+    public void btnLogin()
+    {
+        if (uname.text != "" && pass.text != "" && PlayerPrefs.GetString("uname") == uname.text && PlayerPrefs.GetString("pass") == pass.text)
+        {
+            Deactivate_Object(loginBtn);
+            Deactivate_Object(signinBtn);
+            Deactivate_Object(loginObj);
+        }
+        else {
+
+            warnLogin.text = "*incorrect information";
+        };
+    }
+    public void btnSignin()
+    {
+        if (rUname.text!=""&& rPass.text != ""&& rPassConfirm.text != "" && rPass.text == rPassConfirm.text ) {
+            PlayerPrefs.SetString("uname", rUname.text);
+            PlayerPrefs.SetString("pass", rPass.text);
+            Deactivate_Object(loginBtn);
+            Deactivate_Object(signinBtn);
+            Deactivate_Object(signinObj);
+        }
+        else
+        {
+            warning.text = "*recheck the information";
+        }
     }
 }
 
